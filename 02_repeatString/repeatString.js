9e11191ac35ed7b5e913506ec0a428ed62731c31
emily_m <morin.emily@gmail.com>
2022-10-09 18:54:13 -0400

    Fill in functionality to repeat string, remove skips to run all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = string;
+    if (num > 0) {
+        for (let i = 1; i < num; i++) {
+            result += string;
+        }
+        return result;
+    } else if (num < 0) {
+        return 'ERROR';
+    } else {
+        return '';
+    }
 };
 
 // Do not edit below this line
