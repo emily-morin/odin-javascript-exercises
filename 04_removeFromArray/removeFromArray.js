65583b7a92582a46520a870f1f054d6a099f4c54
emily_m <morin.emily@gmail.com>
2022-10-24 20:20:07 -0400

    Add exercise 4 solution after passing all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    const args = [...arguments];
+    for (let arg of args) {
+        if (array.includes(arg) && args.indexOf(arg) > 0) {
+            const index = array.indexOf(arg);
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
