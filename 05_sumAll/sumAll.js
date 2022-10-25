c82d79ada67402a7fd7d84c0f8771a7590f16869
emily_m <morin.emily@gmail.com>
2022-10-25 14:24:49 -0400

    Add exercise 5 solution after passing all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    } else if (typeof(a) !== "number" || typeof(b) !== "number") {
+        return 'ERROR';
+    } else if (b < a) {
+        let total = 0;
+        for (let i = b; i <= a; i++) {
+            total += i;
+        }
+        return total;
+    } else {
+        let total = 0;
+        for (let i = a; i <= b; i++) {
+            total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
