027e28d74a429d93d710506ee5414239bc398193
emily_m <morin.emily@gmail.com>
2022-10-25 15:51:38 -0400

    Add exercise 7 solution after passing all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5/9;
+  return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const fahrenheit = (celsius * 9/5) + 32;
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
